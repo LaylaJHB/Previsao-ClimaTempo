6f4b233b1acbd20b87b9c9a3c251515cbf61a9b9
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2022-09-20 11:40:43 -0300

    Chave da API do site: https://openweathermap.org/

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 function App() {
 
   const [coordenation, setCoordination] = useState(false);
+  const [weather, setWeather] = useState(false);
+
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
